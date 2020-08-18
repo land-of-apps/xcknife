1cda0b72ad73deba3ecd80d7388a106f12505c17
Jared Kreppein <jared.kreppein@du.edu>
2020-08-18 14:43:48 -0600

    init commit

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,6 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+#$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+
+require 'appmap/rspec'
 require 'xcknife'
 require 'set'
 require 'pp'
--- xcknife.gemspec
+++ xcknife.gemspec
@@ -1,6 +1,7 @@
 # coding: utf-8
 lib = File.expand_path('../lib', __FILE__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+require 'appmap'
 require 'xcknife'
 
 Gem::Specification.new do |s|
